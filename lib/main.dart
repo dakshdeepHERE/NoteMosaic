133ff015a5d38b5931cca809606db3e5e35bebdc
Daksha Deep <dakshadeep1234@gmail.com>
2023-08-28 22:42:06 +0530

    added the Log extensions for isRetrievedFromCache

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+// ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names
+
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() {
   runApp(
@@ -14,7 +22,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       debugShowMaterialGrid: false,
-      home: const HomePage(),
+      home: BlocProvider(
+        create: (context) => PersonsBloc(),
+        child: HomePage(),
+      ),
     ),
   );
 }
@@ -43,13 +54,6 @@ extension UrlString on PersonUrl {
   }
 }
 
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((resp) => resp.transform(utf8.decoder).join())
-    .then((str) => json.decode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
 @immutable
 class Person {
   final String name;
@@ -60,6 +64,53 @@ class Person {
   Person.fromJson(Map<String, dynamic> json)
       : name = json['name'] as String,
         age = json['age'] as int;
+  @override
+  String toString() => 'Person(name=$name,age=$age)';
+}
+
+Future<Iterable<Person>> getPersons(String url) => HttpClient()
+    .getUrl(Uri.parse(url))
+    .then((req) => req.close())
+    .then((resp) => resp.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List<dynamic>)
+    .then((list) => list.map((e) => Person.fromJson(e)));
+
+@immutable
+class FetchResult {
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+
+  const FetchResult(
+      {required this.persons, required this.isRetrievedFromCache});
+  @override
+  String toString() =>
+      'FetchResult(isRetrievedFromCache=$isRetrievedFromCache,persons=$persons)';
+}
+
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<PersonUrl, Iterable<Person>> _cache = {};
+  PersonsBloc() : super(null) {
+    on<LoadPersonsAction>((event, emit) async {
+      final url = event.url;
+      if (_cache.containsKey(url)) {
+        //we have the value in the cache
+        final cachedPersons = _cache[url]!;
+        final result =
+            FetchResult(persons: cachedPersons, isRetrievedFromCache: true);
+        emit(result);
+      } else {
+        final persons = await getPersons(url.urlString);
+        _cache[url] = persons;
+        final result =
+            FetchResult(persons: persons, isRetrievedFromCache: false);
+        emit(result);
+      }
+    });
+  }
+}
+
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index) : null;
 }
 
 class HomePage extends StatelessWidget {
@@ -70,6 +121,55 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.read<PersonsBloc>().add(
+                        const LoadPersonsAction(url: PersonUrl.persons1),
+                      );
+                },
+                child: const Text('Load Json #1'),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.read<PersonsBloc>().add(
+                        const LoadPersonsAction(url: PersonUrl.persons2),
+                      );
+                },
+                child: const Text(
+                  'Load Json #2',
+                ),
+              ),
+            ],
+          ),
+          BlocBuilder<PersonsBloc, FetchResult?>(
+            buildWhen: (previousResult, currentResult) {
+              return previousResult?.persons != currentResult?.persons;
+            },
+            builder: ((context, FetchResult) {
+              FetchResult?.log();
+              final persons = FetchResult?.persons;
+              if (persons == null) {
+                return const SizedBox();
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: persons.length,
+                  itemBuilder: (context, index) {
+                    final person = persons[index]!;
+                    return ListTile(
+                      title: Text(person.name),
+                    );
+                  },
+                ),
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
